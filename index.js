9408fc728ec02cc1c99935a2b9ebb3fd6bbd4e3c
abir718 <abir.hasan2003s@gmail.com>
2025-05-05 22:09:52 +0600

    admin & jwt added

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const cors = require('cors');
 require('dotenv').config();
 const app = express();
 const port = process.env.PORT || 5000;
-
+const jwt = require('jsonwebtoken');
 
 app.use(cors());
 app.use(express.json());
@@ -31,6 +31,38 @@ async function run() {
     const database = client.db("servifyDB")
     const addService = database.collection("addService")
     const addReviews = database.collection("addReviews")
+    const addUsers = database.collection("addUsers")
+
+    app.post('/jwt', async (req, res) => {
+      const user = req.body;
+      const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1h' });
+      res.send({ token });
+    })
+
+    const verifyToken = (req, res, next) => {
+      if (!req.headers.authorization) {
+        return res.status(401).send({ message: 'unauthorized access' });
+      }
+      const token = req.headers.authorization.split(' ')[1];
+      jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+        if (err) {
+          return res.status(401).send({ message: 'unauthorized access' })
+        }
+        req.decoded = decoded;
+        next();
+      })
+    }
+
+    const verifyAdmin = async (req, res, next) => {
+      const email = req.decoded.email;
+      const query = { email: email };
+      const user = await userCol.findOne(query);
+      const isAdmin = user?.role === 'admin';
+      if (!isAdmin) {
+        return res.status(403).send({ message: 'forbidden access' });
+      }
+      next();
+    }
 
     app.post('/services', async (req, res) => {
       const newService = req.body;
@@ -41,6 +73,19 @@ async function run() {
 
     app.get('/services', async (req, res) => {
       const cursor = addService.find();
+      const result = await cursor.toArray();
+      res.send(result);
+    })
+
+    app.post('/users', async (req, res) => {
+      const newUser = req.body;
+      console.log(newUser);
+      const result = await addUsers.insertOne(newUser);
+      res.send(result);
+    })
+
+    app.get('/users', async (req, res) => {
+      const cursor = addUsers.find();
       const result = await cursor.toArray();
       res.send(result);
     })
@@ -65,7 +110,7 @@ async function run() {
       const options = {upsert: false};
       const updatedService = req.body
       const updateDoc = {
-        $set: {image: updatedService.image , title: updatedService.title , name: updatedService.name , website: updatedService.website , description: updatedService.description , price: updatedService.price}
+        $set: {image: updatedService.image , title: updatedService.title , number: updatedService.number , website: updatedService.website , description: updatedService.description , price: updatedService.price}
       };
       const result = await addService.updateOne(filter , updateDoc  , options)
       res.send(result);
